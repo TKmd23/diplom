b27be22e14273101a15ef605468a8790e5ea538b
TKmd23 <86284463+TKmd23@users.noreply.github.com>
2021-07-22 11:33:14 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let upbtn = document.querySelector(".up-button");
+window.onscroll = function(){
+	if(window.pageYOffset > 200){
+		upbtn.classList.add("shown");
+	} else {
+		upbtn.classList.remove("shown");
+	}
+};
+
+upbtn.addEventListener("click", function(){
+	window.scrollTo(0,0);
+});
+
